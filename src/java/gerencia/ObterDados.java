8b7a5dfc7f5d059afb51b96b559787d9c14829bc
Chrysthian Moizes <chrysthianmoizes@gmail.com>
2018-10-24 13:38:15 -0300

    Verificação de ano e semestre disponivel

--- src/java/gerencia/ObterDados.java
+++ src/java/gerencia/ObterDados.java
@@ -3,6 +3,10 @@ package gerencia;
 import dao.AulaDao;
 import dao.ProfessorDao;
 import dao.TurmaDao;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import org.json.JSONArray;
 
@@ -11,7 +15,8 @@ public class ObterDados {
     public static JSONArray getAulasTurma(int ano, int semestre, int idTurma){
             
         try { 
-            return AulaDao.buscarAulasTurma(ano, semestre, idTurma); 
+            JSONArray array = AulaDao.buscarAulasTurma(ano, semestre, idTurma);
+            return ObterDados.verificarAnoSemestreDisponivel(ano, semestre, array);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             return null;
@@ -21,11 +26,38 @@ public class ObterDados {
     public static JSONArray getAulasProfessor(int ano, int semestre, int idProfessor, String nome){
             
         try { 
-            return AulaDao.buscarAulasProfessor(ano, semestre, idProfessor, nome); 
+            JSONArray array = AulaDao.buscarAulasProfessor(ano, semestre, idProfessor, nome);
+            return ObterDados.verificarAnoSemestreDisponivel(ano, semestre, array);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             return null;
         }
+    }
+    
+    public static JSONArray verificarAnoSemestreDisponivel(int ano, int semestre, JSONArray array){
+        
+        try {
+            FileReader file = new FileReader("C:\\Users\\chrys\\Desktop\\arquivo.txt");
+            BufferedReader bf = new BufferedReader(file);
+            String linha = bf.readLine();
+
+            String[] vet = linha.split("/");
+
+            int anoArquivo = Integer.valueOf(vet[0]);
+            int semestreArquivo = Integer.valueOf(vet[1]);
+            
+            if(anoArquivo >= ano && semestreArquivo >= semestre)
+                return array;
+            
+            return new JSONArray();
+                 
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return null;
     }
     
     public static String[] getProfessorPorEmail(String email){
